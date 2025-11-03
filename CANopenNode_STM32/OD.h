3dccf99c54ee6a304510e958af5d845d9478cc68
Ramb0t <2318303+ramb0t@users.noreply.github.com>
2025-11-03 17:48:30 +0000

    CombiLift V5 OD for V1.1.4FW

--- CANopenNode_STM32/OD.h
+++ CANopenNode_STM32/OD.h
@@ -12,11 +12,11 @@
     File info:
         File Names:   OD.h; OD.c
         Project File: CO_FLI_COMBI_profile.xpd
-        File Version: 4
+        File Version: 5
 
         Created:      10/1/2023 1:00:00 PM
         Created By:   RBM
-        Modified:     11/3/2025 7:09:14 PM
+        Modified:     11/3/2025 7:09:46 PM
         Modified By:  RBM
 
     Device Info:
